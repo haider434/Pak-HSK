d7d0a10953fc2a2bc5e893acc5c4efd69865c5b9
haider434 <122698445+haider434@users.noreply.github.com>
2024-03-25 14:50:43 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './component/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      screens: {
+        'normal': '900px',
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  'mobile':'280px',
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
+   
+   
+  },
+  plugins: [],
+}
